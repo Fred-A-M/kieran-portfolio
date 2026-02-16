fcace29b40ea2dc7bcdb3e37dbbebe9ed2350e92
Fred Moore <frederic_archer@hotmail.com>
2026-02-16 18:23:27 +1100

    cursor size

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,7 +6,7 @@ export default function About() {
   return (
     <FadeInPage>
       <div className="relative w-screen min-h-dvh flex justify-center">
-          <div className="flex flex-col items-center justify-center pt-10">
+          <div className="flex flex-col items-center justify-center pt-15 sm:pt-10">
             {profile.background.map((line, index) => (
               <p className='pb-5 w-[80%] lg:w-[50%] 2xl:w-[40%] whitespace-pre-line' key={index}>{line}</p>
             ))}
